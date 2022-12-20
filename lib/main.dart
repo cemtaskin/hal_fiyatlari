dc0b94e5b955c6610063661a5bb25e0d05de9da3
cemtaskin <cemtaskin82@gmail.com>
2022-12-20 19:44:06 +0300

    ARAMA EKLENDİ :)

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,21 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:hal_fiyatlari/screens/price_list_screen.dart';
 
 void main() {
+  HttpOverrides.global = new MyHttpOverrides();
+
   runApp(const MyApp());
+}
+
+class MyHttpOverrides extends HttpOverrides {
+  @override
+  HttpClient createHttpClient(SecurityContext? context) {
+    return super.createHttpClient(context)
+      ..badCertificateCallback =
+          (X509Certificate cert, String host, int port) => true;
+  }
 }
 
 class MyApp extends StatelessWidget {
--- lib/screens/price_list_screen.dart
+++ lib/screens/price_list_screen.dart
@@ -1,6 +1,9 @@
 import 'dart:convert';
 
+import 'package:date_time_picker/date_time_picker.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:hal_fiyatlari/main.dart';
 import 'package:hal_fiyatlari/model/hal_fiyat_listesi.dart';
 import 'package:hal_fiyatlari/model/hal_fiyatlari.dart';
 import 'package:http/http.dart' as http;
@@ -14,20 +17,72 @@ class PriceListScreen extends StatefulWidget {
 
 class _PriceListScreenState extends State<PriceListScreen> {
   bool _isWorking = true;
+  String _selectedDate = '';
+  var url =
+      Uri.https('openapi.izmir.bel.tr', 'api/ibb/halfiyatlari/sebzemeyve/');
+
+  Future<HalFiyatlari> getFromWebService() async {
+    var url = Uri.https('openapi.izmir.bel.tr',
+        'api/ibb/halfiyatlari/sebzemeyve/' + _selectedDate);
+
+    var value = await http.get(url);
+    return HalFiyatlari.fromJson(jsonDecode(value.body));
+  }
 
   HalFiyatlari halFiyatlari = HalFiyatlari();
+  TextEditingController controller = TextEditingController();
+
   @override
   void initState() {
     super.initState();
 
-    var url = Uri.https(
-        'openapi.izmir.bel.tr', 'api/ibb/halfiyatlari/sebzemeyve/2022-01-15');
-    http.get(url).then((value) {
-      halFiyatlari = HalFiyatlari.fromJson(jsonDecode(value.body));
+    DateTime today = DateTime.now();
+    _selectedDate = "${today.year}-${today.month}-${today.day}";
+
+    getFromWebService().then((value) {
       setState(() {
         _isWorking = false;
+        halFiyatlari = value;
       });
     });
+  }
+
+  Future<void> _showMyDialog() async {
+    return showDialog<void>(
+      context: context,
+      barrierDismissible: false, // user must tap button!
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('AlertDialog Title'),
+          content: SingleChildScrollView(
+            child: DateTimePicker(
+              initialValue: _selectedDate,
+              firstDate: DateTime(2000),
+              lastDate: DateTime(2100),
+              dateLabelText: 'Date',
+              onChanged: (val) {
+                Navigator.of(context).pop();
+                _selectedDate = val;
+                setState(() {
+                  _isWorking = true;
+                });
+                getFromWebService().then((value) {
+                  setState(() {
+                    _isWorking = false;
+                    halFiyatlari = value;
+                  });
+                });
+              },
+              validator: (val) {
+                print(val);
+                return null;
+              },
+              onSaved: (val) => print(val),
+            ),
+          ),
+        );
+      },
+    );
   }
 
   @override
@@ -39,8 +94,8 @@ class _PriceListScreenState extends State<PriceListScreen> {
           actions: [
             InkWell(
               child: Icon(Icons.date_range),
-              onTap: () {
-                print('clicked');
+              onTap: () async {
+                await _showMyDialog();
               },
             )
           ],
@@ -49,17 +104,55 @@ class _PriceListScreenState extends State<PriceListScreen> {
             ? Center(
                 child: CircularProgressIndicator(),
               )
-            : ListView.builder(
-                itemCount: halFiyatlari.halFiyatListesi!.length,
-                itemBuilder: (BuildContext context, int index) {
-                  HalFiyatListesi halFiyatListesi =
-                      halFiyatlari.halFiyatListesi![index];
-                  return ListTile(
-                    leading: const Icon(Icons.list),
-                    title: Text(halFiyatListesi!.malAdi!),
-                    subtitle: Text(halFiyatListesi!.ortalamaUcret! + "TL"),
-                  );
-                }),
+            : Column(
+                children: [
+                  Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+                    child: TextField(
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        hintText: 'ARAMAK İSTEDİĞİNİZ MEYVE/SEBZE',
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    child: ListView.builder(
+                        itemCount: halFiyatlari.halFiyatListesi!.length,
+                        itemBuilder: (BuildContext context, int index) {
+                          HalFiyatListesi halFiyatListesi =
+                              halFiyatlari.halFiyatListesi![index];
+                          return Padding(
+                            padding: const EdgeInsets.all(2.0),
+                            child: Card(
+                              child: Column(
+                                children: [
+                                  Row(
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceBetween,
+                                    children: [
+                                      Text(
+                                        halFiyatListesi!.malAdi!,
+                                        style: TextStyle(fontSize: 20.0),
+                                      ),
+                                      Text(
+                                        halFiyatListesi!.ortalamaUcret!,
+                                        style: TextStyle(fontSize: 20.0),
+                                      ),
+                                    ],
+                                  ),
+                                  Image.network(
+                                    'https://eislem.izmir.bel.tr/YuklenenDosyalar/HalGorselleri/' +
+                                        halFiyatListesi!.gorsel!,
+                                    width: 120,
+                                  ),
+                                ],
+                              ),
+                            ),
+                          );
+                        }),
+                  ),
+                ],
+              ),
       ),
     );
   }
